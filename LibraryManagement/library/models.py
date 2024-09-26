eabe415dadb313639ae1c4a076781c03be8d68eb
lawrenceChege <lawrencechege.s@gmail.com>
2024-09-27 00:51:15 +0300

    initial setup

---
+++ LibraryManagement/library/models.py
@@ -0,0 +1,11 @@
+# from django.db import models
+# from django.utils import timezone
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=250)
+#     author = models.CharField(max_length=200)
+#     isbn = models.CharField(max_length=20, unique=True)
+#     quantity= models.PositiveIntegerField(default=0)
+#     available= models.PositiveIntegerField(default=0)
+#     publisher= models.CharField(max_length=250)
+#     publishing_date = models.DateTimeField()
