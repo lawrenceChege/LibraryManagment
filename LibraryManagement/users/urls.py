882a3f956c8031a2958b485624a6e4c684b7fbfb
lawrenceChege <lawrencechege.s@gmail.com>
2024-09-27 04:18:31 +0300

    add register new user

--- LibraryManagement/users/urls.py
+++ LibraryManagement/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path, include
-from users.views import dashboard
+from users.views import dashboard, register
 
 
 urlpatterns = [
     re_path(r"^users/", include("django.contrib.auth.urls")),
-    re_path(r"dashboard/", dashboard, name="dashboard"),
+    re_path(r"dashboard/", dashboard, name="dashboard"),    
+    re_path(r"register/", register, name="register"),
 ]
